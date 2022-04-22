54169076eccb768683d06ee0f25f791a17bb16c1
daec <daescarcega@gmail.com>
2022-04-22 14:45:39 -0500

    Commit con Splashscreen

--- app/src/main/java/edu/itesm/gastos/SplashActivity.kt
+++ app/src/main/java/edu/itesm/gastos/SplashActivity.kt
@@ -9,11 +9,53 @@ import edu.itesm.gastos.activities.MainActivity
 
 class SplashActivity : AppCompatActivity() {
 
+    private lateinit var motionLayout : MotionLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        controlaSplsh()
 
 
+
+    }
+
+    fun controlaSplsh(){
+        motionLayout = findViewById(R.id.splash_main)
+        motionLayout.startLayoutAnimation()
+        motionLayout.setTransitionListener(object : MotionLayout.TransitionListener{
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int
+            ) {
+
+            }
+
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
+
+            }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {
+
+            }
+
+        })
     }
 }
